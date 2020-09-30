09059754c2e654fdbded1392d24a57cb14b4154f
turag <turag@LAPTOP-EU7APILU>
2020-09-30 11:41:59 +0200

    Aggiungere i file di progetto.

---
+++ ITSUmbria2020.GameOnline/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITSUmbria2020.GameOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
